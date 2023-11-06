2a4d8d6bce96d97d06819add77c56e6d667dec81
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-11-06 13:47:46 +0100

    Update - add newEmployee in state

--- src/Components/CreateEmployee/index.jsx
+++ src/Components/CreateEmployee/index.jsx
@@ -4,7 +4,8 @@ import Select from 'react-select';
 import { useDispatch } from 'react-redux';
 import { addEmployee } from '../../slices/employeeSlice';
 import * as datipiReact from 'datipi-react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { states } from '../../data/states';
 
 /**
  * Component representing the Create Employee page.
@@ -13,24 +14,41 @@ import { useEffect } from 'react';
  */
 const CreateEmployee = () => {
 
+  // Utilise useState pour gérer les valeurs des champs de saisie
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+  const [selectedState, setSelectedState] = useState(null);
+  const [zipCode, setZipCode] = useState('');
+  const [selectedDepartment, setSelectedDepartment] = useState(null);
   /**
    * A hook that initializes DateTimePickers when the component mounts
    * and destroys them when the component unmounts.
    */
   useEffect(() => {
-    datipiReact.initDateTimePicker('#date-of-birth', {
-      format: 'Y-m-d',
-    });
+    datipiReact.initDateTimePicker(
+      '#date-of-birth',
+      {
+        format: 'Y-m-d'
+      },
+      setDateOfBirth);
 
-    datipiReact.initDateTimePicker('#start-date', {
-      format: 'Y-m-d',
-    });
+    datipiReact.initDateTimePicker(
+      '#start-date',
+      {
+        format: 'Y-m-d'
+      },
+      setStartDate);
 
     return () => {
       datipiReact.destroyDateTimePicker('#date-of-birth');
       datipiReact.destroyDateTimePicker('#start-date');
     };
   }, []);
+
 
   /**
    * Handles the save action for creating a new employee.
@@ -39,249 +57,50 @@ const CreateEmployee = () => {
   const dispatch = useDispatch();
   const handleSave = (event) => {
     event.preventDefault();
-    dispatch(addEmployee({
-      // Employee details you want to add
-    }));
+    const newEmployee = {
+      firstName,
+      lastName,
+      dateOfBirth,
+      startDate,
+      street,
+      city,
+      state: selectedState ? selectedState.value : null,
+      zipCode,
+      department: selectedDepartment ? selectedDepartment.value : null,
+    };
+    console.log('État mis à jour :', newEmployee);
+
+    dispatch(addEmployee(newEmployee));
   };
 
-  const states = [
-    {
-      "name": "Alabama",
-      "abbreviation": "AL"
-    },
-    {
-      "name": "Alaska",
-      "abbreviation": "AK"
-    },
-    {
-      "name": "American Samoa",
-      "abbreviation": "AS"
-    },
-    {
-      "name": "Arizona",
-      "abbreviation": "AZ"
-    },
-    {
-      "name": "Arkansas",
-      "abbreviation": "AR"
-    },
-    {
-      "name": "California",
-      "abbreviation": "CA"
-    },
-    {
-      "name": "Colorado",
-      "abbreviation": "CO"
-    },
-    {
-      "name": "Connecticut",
-      "abbreviation": "CT"
-    },
-    {
-      "name": "Delaware",
-      "abbreviation": "DE"
-    },
-    {
-      "name": "District Of Columbia",
-      "abbreviation": "DC"
-    },
-    {
-      "name": "Federated States Of Micronesia",
-      "abbreviation": "FM"
-    },
-    {
-      "name": "Florida",
-      "abbreviation": "FL"
-    },
-    {
-      "name": "Georgia",
-      "abbreviation": "GA"
-    },
-    {
-      "name": "Guam",
-      "abbreviation": "GU"
-    },
-    {
-      "name": "Hawaii",
-      "abbreviation": "HI"
-    },
-    {
-      "name": "Idaho",
-      "abbreviation": "ID"
-    },
-    {
-      "name": "Illinois",
-      "abbreviation": "IL"
-    },
-    {
-      "name": "Indiana",
-      "abbreviation": "IN"
-    },
-    {
-      "name": "Iowa",
-      "abbreviation": "IA"
-    },
-    {
-      "name": "Kansas",
-      "abbreviation": "KS"
-    },
-    {
-      "name": "Kentucky",
-      "abbreviation": "KY"
-    },
-    {
-      "name": "Louisiana",
-      "abbreviation": "LA"
-    },
-    {
-      "name": "Maine",
-      "abbreviation": "ME"
-    },
-    {
-      "name": "Marshall Islands",
-      "abbreviation": "MH"
-    },
-    {
-      "name": "Maryland",
-      "abbreviation": "MD"
-    },
-    {
-      "name": "Massachusetts",
-      "abbreviation": "MA"
-    },
-    {
-      "name": "Michigan",
-      "abbreviation": "MI"
-    },
-    {
-      "name": "Minnesota",
-      "abbreviation": "MN"
-    },
-    {
-      "name": "Mississippi",
-      "abbreviation": "MS"
-    },
-    {
-      "name": "Missouri",
-      "abbreviation": "MO"
-    },
-    {
-      "name": "Montana",
-      "abbreviation": "MT"
-    },
-    {
-      "name": "Nebraska",
-      "abbreviation": "NE"
-    },
-    {
-      "name": "Nevada",
-      "abbreviation": "NV"
-    },
-    {
-      "name": "New Hampshire",
-      "abbreviation": "NH"
-    },
-    {
-      "name": "New Jersey",
-      "abbreviation": "NJ"
-    },
-    {
-      "name": "New Mexico",
-      "abbreviation": "NM"
-    },
-    {
-      "name": "New York",
-      "abbreviation": "NY"
-    },
-    {
-      "name": "North Carolina",
-      "abbreviation": "NC"
-    },
-    {
-      "name": "North Dakota",
-      "abbreviation": "ND"
-    },
-    {
-      "name": "Northern Mariana Islands",
-      "abbreviation": "MP"
-    },
-    {
-      "name": "Ohio",
-      "abbreviation": "OH"
-    },
-    {
-      "name": "Oklahoma",
-      "abbreviation": "OK"
-    },
-    {
-      "name": "Oregon",
-      "abbreviation": "OR"
-    },
-    {
-      "name": "Palau",
-      "abbreviation": "PW"
-    },
-    {
-      "name": "Pennsylvania",
-      "abbreviation": "PA"
-    },
-    {
-      "name": "Puerto Rico",
-      "abbreviation": "PR"
-    },
-    {
-      "name": "Rhode Island",
-      "abbreviation": "RI"
-    },
-    {
-      "name": "South Carolina",
-      "abbreviation": "SC"
-    },
-    {
-      "name": "South Dakota",
-      "abbreviation": "SD"
-    },
-    {
-      "name": "Tennessee",
-      "abbreviation": "TN"
-    },
-    {
-      "name": "Texas",
-      "abbreviation": "TX"
-    },
-    {
-      "name": "Utah",
-      "abbreviation": "UT"
-    },
-    {
-      "name": "Vermont",
-      "abbreviation": "VT"
-    },
-    {
-      "name": "Virgin Islands",
-      "abbreviation": "VI"
-    },
-    {
-      "name": "Virginia",
-      "abbreviation": "VA"
-    },
-    {
-      "name": "Washington",
-      "abbreviation": "WA"
-    },
-    {
-      "name": "West Virginia",
-      "abbreviation": "WV"
-    },
-    {
-      "name": "Wisconsin",
-      "abbreviation": "WI"
-    },
-    {
-      "name": "Wyoming",
-      "abbreviation": "WY"
-    }
-  ];
+
+  // Fonctions de gestionnaires d'événements pour mettre à jour les états locaux
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleStreetChange = (event) => {
+    setStreet(event.target.value);
+  };
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleStateChange = (selectedOption) => {
+    setSelectedState(selectedOption);
+  };
+
+  const handleZipCodeChange = (event) => {
+    setZipCode(event.target.value);
+  };
+  const handleDepartmentChange = (selectedOption) => {
+    setSelectedDepartment(selectedOption);
+  };
 
   /**
    * Options for the State dropdown.
@@ -304,11 +123,6 @@ const CreateEmployee = () => {
     { value: 'Legal', label: 'Legal' },
   ];
 
-  const dateTimePickerOptions = {
-    format: 'Y-m-d',
-    // Autres options personnalisées si nécessaire
-  };
-
   return (
     <>
       <div>
@@ -319,37 +133,85 @@ const CreateEmployee = () => {
         <h2>Create Employee</h2>
         <form action="#" id="create-employee">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
 
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            value={lastName}
+            onChange={handleLastNameChange}
+          />
 
           <label htmlFor="date-of-birth">Date of Birth</label>
-          <input id="date-of-birth" type="text" />
+          <input
+            id="date-of-birth"
+            type="text"
+            value={dateOfBirth}
+            onChange={(e) => setDateOfBirth(e.target.value)}
+          />
 
           <label htmlFor="start-date">Start Date</label>
-          <input id="start-date" type="text" />
+          <input
+            id="start-date"
+            type="text"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
 
           <fieldset className="address">
             <legend>Address</legend>
 
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
+            <input
+              id="street"
+              type="text"
+              value={street}
+              onChange={handleStreetChange}
+            />
 
             <label htmlFor="city">City</label>
-            <input id="city" type="text" />
+            <input
+              id="city"
+              type="text"
+              value={city}
+              onChange={handleCityChange}
+            />
 
             <label htmlFor="state">State</label>
-            <Select name="state" id="state" options={options} />
+            <Select
+              name="state"
+              id="state"
+              options={options}
+              value={selectedState}
+              onChange={handleStateChange}
+            />
 
             <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+            <input
+              id="zip-code"
+              type="number"
+              value={zipCode}
+              onChange={handleZipCodeChange}
+            />
           </fieldset>
 
           <label htmlFor="department">Department</label>
-          <Select name="department" id="department" options={departmentOptions} />
+          <Select
+            name="department"
+            id="department"
+            options={departmentOptions}
+            value={selectedDepartment}
+            onChange={handleDepartmentChange}
+          />
         </form>
         <button onClick={handleSave}>Save</button>
+
       </div>
     </>
   );
